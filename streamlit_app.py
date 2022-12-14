5303cfdef1a078ef2c5fd93c775e9fd6ab3db05b
Anurag Pandey <120551758+Anurag231122@users.noreply.github.com>
2022-12-14 12:58:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,11 @@
 import streamlit
 streamlit.title('My parents New Healthy Dinner') 
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+
