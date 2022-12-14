b6a83a411051d9dd5be9a0b59719cd04ba7b35b2
Anurag Pandey <120551758+Anurag231122@users.noreply.github.com>
2022-12-14 13:08:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,7 @@ streamlit.dataframe(my_fruit_list)
 
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
